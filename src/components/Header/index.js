e4f763a8076f5c8b9b69d741848bc3fcc45791fa
dustin george <DustinG27@gmail.com>
2021-12-14 21:51:53 -0500

    basic header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./style.css";
 
 function Header(props) {
-  return <h1 className="title">Test</h1>;
+  return <div className="header">
+      <div className="title">
+          <p>mmm beer</p>
+      </div>
+      <div className="info">
+        <p>Click each beer only once</p>
+      </div>
+      <div className="counter">
+        <p>Current Score: 0</p>
+        <p>High Score: 0</p>
+      </div>
+  </div>;
 }
 
 export default Header;
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper(props) {
+    return <div>
+        <p>dis jawn</p>
+    </div>;
+}
+
+export default Wrapper;
